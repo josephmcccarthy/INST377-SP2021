a89a50205b203cc3c2129c17e423778f6fee7ebd
joseph mccarthy <jem2018@umd.edu>
2021-02-11 17:51:25 -0500

    lab 03

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,19 @@
-/* Put your javascript in here */
+let width = 130;
+let count = 3;
+
+let list = carousel.querySelector("ul");
+let listElems = carousel.querySelectorAll("li");
+
+let position = 0;
+
+carousel.querySelector(".back").onclick = function () {
+  position += width * count;
+  position = Math.min(position, 0);
+  list.style.marginLeft = position + "px";
+};
+
+carousel.querySelector(".next").onclick = function () {
+  position -= width * count;
+  position = Math.max(position, -width * (listElems.length - count));
+  list.style.marginLeft = position + "px";
+};
